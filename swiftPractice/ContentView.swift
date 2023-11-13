41788f8b3155795d9bdc41e8fe7f02c4f88bdb37
Suhail Ahmad <97229491+ClawedCatalyst@users.noreply.github.com>
2023-11-14 02:23:42 +0530

    Added UI for a weather screen (#1)

--- swiftPractice/ContentView.swift
+++ swiftPractice/ContentView.swift
@@ -8,28 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var email = ""
-    @State private var password = ""
-    
     var body: some View {
-        
-        VStack{
-            HStack{
-                Text("Item 1")
-                Text("Item 2")
-                Text("Item 3")
-            }
-            
-            HStack{
-                Text("Item 1")
-                Text("Item 2")
-                Text("Item 3")
-            }
-            
-            ZStack {
-                Text("Back")
-                Text("Front")
+        ZStack {
+            LinearGradient(
+                colors: [Color.blue, Color.white],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea(.all)
+
+            VStack {
+                Text("Delhi, IN")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding(10)
+
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                .padding(.bottom, 60)
+
+                HStack(spacing: 15) {
+                    ExtractedView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 73)
+                    ExtractedView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 73)
+                    ExtractedView(dayOfWeek: "THUS", imageName: "wind.snow", temperature: 55)
+                    ExtractedView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
+                    ExtractedView(dayOfWeek: "SAT", imageName: "cloud.rain.fill", temperature: 25)
+                }
+                .padding(.bottom, 60)
+
+                Button("Change Time of the Day") {
+                    // Handle button action
+                }
+                .frame(width: 300, height: 50)
+                .background(Color.white)
+                .foregroundColor(.blue)
+                .font(.system(size: 18, weight: .bold))
+                .cornerRadius(10)
+
+                Spacer()
             }
         }
     }
@@ -39,3 +64,26 @@ struct ContentView: View {
     ContentView()
 }
 
+
+struct ExtractedView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 22, weight: .medium))
+                .foregroundColor(.white).padding(5)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 22, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
