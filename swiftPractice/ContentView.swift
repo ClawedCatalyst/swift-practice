a04686b8dec0e2e12627ee1dd7f92495024a2d4a
personal <suhaillahmadd0@gmail.com>
2023-11-15 01:07:32 +0530

    iOS. 16 update ContainerRelativeShape

--- swiftPractice/ContentView.swift
+++ swiftPractice/ContentView.swift
@@ -91,11 +91,15 @@ struct BackgroundView: View {
     @Binding var isNight: Bool
     
     var body: some View {
-        LinearGradient(
-            colors: [isNight ? Color.black : Color.blue, isNight ? Color.gray : Color.white],
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
-        .ignoresSafeArea()
+//        LinearGradient(
+//            colors: [isNight ? Color.black : Color.blue, isNight ? Color.gray : Color.white],
+//            startPoint: .topLeading,
+//            endPoint: .bottomTrailing
+//        )
+//        .ignoresSafeArea()
+        
+            ContainerRelativeShape()
+            .fill(isNight ? Color.black.gradient : Color.blue.gradient)
+            .ignoresSafeArea()
     }
 }
